5d2741a0c685d0cdd10cc2e00a021a6e3aa7d088
YungVenuz <5196666qwe@gmail.com>
2022-06-24 15:40:16 +0000

    新增: 动画相关

--- 主程序.py
+++ 主程序.py
@@ -17,31 +17,31 @@ pygame.init()
 屏幕 = pygame.display.set_mode((宽度, 高度))
 
 
-
-背景图片 = pygame.image.load('assets/images/background/background.jpg').convert_alpha()
+背景图片 = pygame.image.load(
+    'assets/images/background/background.jpg').convert_alpha()
 
 
 战士大小 = 162
 战士缩放 = 4
 战士偏移量 = [72, 56]
-战士数据 = [ 战士大小, 战士缩放, 战士偏移量]
+战士数据 = [战士大小, 战士缩放, 战士偏移量]
 巫师大小 = 250
 巫师缩放 = 3
 巫师偏移量 = [112, 107]
-巫师数据 = [ 巫师大小, 巫师缩放, 巫师偏移量]
+巫师数据 = [巫师大小, 巫师缩放, 巫师偏移量]
 
 
 # 加载精灵地图
 # warrior 战士
-战士序列图 = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+战士序列图 = pygame.image.load(
+    "assets/images/warrior/Sprites/warrior.png").convert_alpha()
 # wizard 巫师
-巫师序列图 = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
-
+巫师序列图 = pygame.image.load(
+    "assets/images/wizard/Sprites/wizard.png").convert_alpha()
 
 
 战士动画步骤 = [10, 8, 1, 7, 7, 3, 7]
 巫师动画步骤 = [8, 8, 1, 8, 8, 3, 7]
-
 
 
 运行 = True
@@ -50,15 +50,18 @@ pygame.init()
 红色 = (255, 0, 0)
 白色 = (255, 255, 255)
 
+
 def 绘制背景():
-	缩放的图片 = pygame.transform.scale(背景图片, (宽度, 高度))
-	屏幕.blit(缩放的图片, (0, 0))
+    缩放的图片 = pygame.transform.scale(背景图片, (宽度, 高度))
+    屏幕.blit(缩放的图片, (0, 0))
+
 
 def 绘制血量(生命值, x, y):
-	比例 = 生命值 / 10000
-	pygame.draw.rect(屏幕, 白色, (x-2, y-3, 404, 36))
-	pygame.draw.rect(屏幕, 红色, (x, y, 400, 30))
-	pygame.draw.rect(屏幕, 绿色, (x, y, 400 * 比例, 30))
+    比例 = 生命值 / 10000
+    pygame.draw.rect(屏幕, 白色, (x - 2, y - 3, 404, 36))
+    pygame.draw.rect(屏幕, 红色, (x, y, 400, 30))
+    pygame.draw.rect(屏幕, 绿色, (x, y, 400 * 比例, 30))
+
 
 # 类的实例化
 玩家1 = 玩家(100, 310, False, 战士数据, 战士序列图, 战士动画步骤)
@@ -67,27 +70,31 @@ def 绘制血量(生命值, x, y):
 
 while 运行:
 
-	时钟.tick(帧数)
+    时钟.tick(帧数)
 
-	绘制背景()
+    绘制背景()
 
-	绘制血量(玩家1.生命值, 20, 20)
-	绘制血量(玩家2.生命值, 570, 20)
+    绘制血量(玩家1.生命值, 20, 20)
+    绘制血量(玩家2.生命值, 570, 20)
 
-	# 使用玩家1的绘制方法
-	玩家1.绘制(屏幕)
-	玩家1.移动(宽度, 高度, 屏幕, 玩家2)
-	玩家2.绘制(屏幕)
-	# 玩家2.移动(宽度, 高度, 屏幕, 玩家1)
+    玩家1.移动(宽度, 高度, 屏幕, 玩家2)
 
-	# 获取游戏里面的事件, 包括键盘, 鼠标, 关闭等等事件
-	for 事件 in pygame.event.get():
-		# 如果事件的类型等于关闭的话, 执行
-		if 事件.type == pygame.QUIT:
+    玩家1.更新()
+    玩家2.更新()
 
-			运行 = False
-	# 刷新事件
-	pygame.display.update()
+    # 使用玩家1的绘制方法
+    玩家1.绘制(屏幕)
+    玩家2.绘制(屏幕)
+    # 玩家2.移动(宽度, 高度, 屏幕, 玩家1)
+
+    # 获取游戏里面的事件, 包括键盘, 鼠标, 关闭等等事件
+    for 事件 in pygame.event.get():
+        # 如果事件的类型等于关闭的话, 执行
+        if 事件.type == pygame.QUIT:
+
+            运行 = False
+    # 刷新事件
+    pygame.display.update()
 
 # 退出游戏
 pygame.quit()
--- 玩家.py
+++ 玩家.py
@@ -3,123 +3,194 @@ import pygame
 
 class 玩家:
 
-	# 构造函数
-	def __init__(self, x, y, 翻转, 数据, 精灵序列图, 动画步骤列表):
+    # 构造函数
+    def __init__(self, x, y, 翻转, 数据, 精灵序列图, 动画步骤列表):
 
-		self.矩形 = pygame.Rect(x, y, 80, 180)
-		self.y速度 = 0
-		self.跳跃 = False
-		self.攻击类型 = 0
-		self.攻击中 = False
-		self.生命值 = 10000
-		self.图片翻转 = 翻转
+        self.矩形 = pygame.Rect(x, y, 80, 180)
+        self.y速度 = 0
+        self.跳跃 = False
+        self.攻击类型 = 0
+        self.攻击中 = False
+        self.生命值 = 10000
+        self.图片翻转 = 翻转
 
-		# 图片相关
-		self.大小 = 数据[0]
-		self.缩放 = 数据[1]
-		self.偏移 = 数据[2]
-		self.动画列表 = self.加载图片(精灵序列图, 动画步骤列表)
-		self.动作 = 0 # 0 站立 1 奔跑 2 跳跃 3 进攻1 4 进攻2 5 受伤 6 死亡
-		self.帧索引  = 0
-		self.图片 = self.动画列表[self.动作][self.帧索引]
+        # 图片相关
+        self.大小 = 数据[0]
+        self.缩放 = 数据[1]
+        self.偏移 = 数据[2]
+        self.动画列表 = self.加载图片(精灵序列图, 动画步骤列表)
+        self.动作 = 0  # 0 站立 1 奔跑 2 跳跃 3 进攻1 4 进攻2 5 受伤 6 死亡
+        self.帧索引 = 0
+        self.图片 = self.动画列表[self.动作][self.帧索引]
 
+        # 更新时间
+        self.更新时间 = pygame.time.get_ticks()
+        self.奔跑 = False
+        self.攻击冷却 = 0
+        self.被攻击 = False
+        self.存活 = True
 
-	def 加载图片(self, 精灵序列图, 动画步骤列表):
+    def 加载图片(self, 精灵序列图, 动画步骤列表):
 
-		动画列表 = []
+        动画列表 = []
 
-		for y, 动画步骤 in enumerate(动画步骤列表):
+        for y, 动画步骤 in enumerate(动画步骤列表):
 
-			临时图片列表 = []
+            临时图片列表 = []
 
-			for x in range(动画步骤):
+            for x in range(动画步骤):
 
-				临时图片 = 精灵序列图.subsurface(x * self.大小,  y * self.大小, self.大小, self.大小)
-				临时图片 = pygame.transform.scale(临时图片, (self.大小 * self.缩放, self.大小 * self.缩放))
-				临时图片列表.append(临时图片)
+                临时图片 = 精灵序列图.subsurface(
+                    x * self.大小, y * self.大小, self.大小, self.大小)
+                临时图片 = pygame.transform.scale(
+                    临时图片, (self.大小 * self.缩放, self.大小 * self.缩放))
+                临时图片列表.append(临时图片)
 
-			动画列表.append(临时图片列表)
+            动画列表.append(临时图片列表)
 
-		return 动画列表
+        return 动画列表
 
+    def 移动(self, 宽度, 高度, 屏幕, 敌人):
 
-	def 移动(self, 宽度, 高度, 屏幕, 敌人):
+        重力 = 2
 
-		重力 = 2
+        速度 = 10
+        x增量 = 0
+        y增量 = 0
+        self.奔跑 = False
+        self.攻击类型 = 0
 
-		速度 = 10
-		x增量 = 0
-		y增量 = 0
+        键盘 = pygame.key.get_pressed()
 
-		键盘 = pygame.key.get_pressed()
+        # if self.攻击中 == False:
+        if 键盘[pygame.K_a]:
+            x增量 = -速度
+            self.奔跑 = True
+        if 键盘[pygame.K_d]:
+            x增量 = 速度
+            self.奔跑 = True
+        if 键盘[pygame.K_w] and self.跳跃 == False:
+            self.y速度 = -30
+            self.跳跃 = True
 
-		# if self.攻击中 == False:
-		if 键盘[pygame.K_a]:
-			x增量 = -速度
-		if 键盘[pygame.K_d]:
-			x增量 = 速度
-		if 键盘[pygame.K_w] and self.跳跃 == False:
-			self.y速度 = -30
-			self.跳跃 = True
+        # 发动攻击
+        if 键盘[pygame.K_j] or 键盘[pygame.K_k]:
+            self.攻击(屏幕, 敌人)
+            if 键盘[pygame.K_j]:
+                self.攻击类型 = 1
+            if 键盘[pygame.K_k]:
+                self.攻击类型 = 2
 
-		# 发动攻击
-		if 键盘[pygame.K_j] or 键盘[pygame.K_k]:
-			self.攻击(屏幕, 敌人)
-			if 键盘[pygame.K_j]:
-				self.攻击类型 = 1
-			if 键盘[pygame.K_k]:
-				self.攻击类型 = 2
+        self.y速度 += 重力
+        y增量 += self.y速度
 
+        # 左右移动  左边界
+        if self.矩形.left + x增量 < 0:
+            x增量 = 0 - self.矩形.left
+        # 左右移动  右边界
+        if self.矩形.right + x增量 > 宽度:
+            x增量 = 0
 
-		self.y速度 += 重力
-		y增量 += self.y速度
+        # 下方判断
+        if self.矩形.bottom + y增量 > 高度 - 110:
+            self.y速度 = 0
+            y增量 = 0
+            self.跳跃 = False
 
+        if 敌人.矩形.centerx > self.矩形.centerx:
+            self.图片翻转 = False
+        else:
+            self.图片翻转 = True
 
-		# 左右移动  左边界
-		if self.矩形.left + x增量 < 0:
-			x增量 = 0 - self.矩形.left
-		# 左右移动  右边界
-		if self.矩形.right + x增量 > 宽度:
-			x增量 = 0
+        if self.攻击冷却 > 0:
+            self.攻击冷却 -= 2
 
-		# 下方判断
-		if self.矩形.bottom + y增量 > 高度 - 110:
-			self.y速度 = 0
-			y增量 = 0
-			self.跳跃 = False
+        self.矩形.x += x增量
+        self.矩形.y += y增量
 
-		if 敌人.矩形.centerx >  self.矩形.centerx:
-			self.图片翻转 = False
-		else:
-			self.图片翻转 = True
+    def 更新(self):
 
-		self.矩形.x += x增量
-		self.矩形.y += y增量
+        # if self.运行:
+        #     self.动作 = 1
+        # else:
+        #     self.动作 = 0
+        if self.生命值 <= 0:
+            self.生命值 = 0
+            self.存活 = False
+            self.更新动作(6)
+        elif self.被攻击:
+            self.更新动作(5)
+        elif self.攻击中:
+            if self.攻击类型 == 1:
+                self.更新动作(3)
+            elif self.攻击类型 == 2:
+                self.更新动作(4)
+        elif self.跳跃:
+            self.更新动作(2)
+        elif self.奔跑:
+            self.更新动作(1)
+        else:
+            self.更新动作(0)
 
-	def 攻击(self, 屏幕, 敌人):
+        动画冷却 = 50  # 半秒
 
-		self.攻击中 = True
+        self.图片 = self.动画列表[self.动作][self.帧索引]
 
-		攻击矩形 = pygame.Rect(
-			self.矩形.centerx - (2 * self.矩形.width * self.图片翻转),
-			self.矩形.y,
-			2 * self.矩形.width,
-			self.矩形.height
-		)
+        if pygame.time.get_ticks() - self.更新时间 > 动画冷却:
 
-		if 攻击矩形.colliderect(敌人.矩形):
-			敌人.生命值 -= 100
+            self.更新时间 = pygame.time.get_ticks()
+            self.帧索引 += 1
 
-		pygame.draw.rect(屏幕, (0, 255, 0), 攻击矩形)
+        if self.帧索引 >= len(self.动画列表[self.动作]):
 
-	def 绘制(self, 屏幕):
-		图片 = pygame.transform.flip(self.图片, self.图片翻转, False)
-		pygame.draw.rect(屏幕, (100, 100, 100), self.矩形)
-		屏幕.blit(图片,
-			(
-				self.矩形.x - int(self.偏移[0] * self.缩放),
-				self.矩形.y - int(self.偏移[1] * self.缩放),
-				# self.矩形.x,
-				# self.矩形.y,
-			)
-		)
+            self.帧索引 = 0
+
+            if self.存活 == False:
+                self.帧索引 = len(self.动画列表[self.动作]) - 1
+            else:
+
+                if self.动作 == 3 or self.动作 == 4:
+                    self.攻击中 = False
+                    self.攻击冷却 = 50
+
+                if self.动作 == 5:
+                    self.被攻击 = False
+                    self.攻击中 = False
+                    self.攻击冷却 = 50
+
+    def 更新动作(self, 新动作):
+        if 新动作 != self.动作:
+            self.动作 = 新动作
+            self.帧索引 = 0
+            self.更新时间 = pygame.time.get_ticks()
+
+    def 攻击(self, 屏幕, 敌人):
+
+        if self.攻击冷却 == 0:
+
+            self.攻击中 = True
+
+            攻击矩形 = pygame.Rect(
+                self.矩形.centerx - (2 * self.矩形.width * self.图片翻转),
+                self.矩形.y,
+                2 * self.矩形.width,
+                self.矩形.height
+            )
+
+            if 攻击矩形.colliderect(敌人.矩形):
+                敌人.生命值 -= 100
+                敌人.被攻击 = True
+
+            pygame.draw.rect(屏幕, (0, 255, 0), 攻击矩形)
+
+    def 绘制(self, 屏幕):
+        图片 = pygame.transform.flip(self.图片, self.图片翻转, False)
+        pygame.draw.rect(屏幕, (100, 100, 100), self.矩形)
+        屏幕.blit(图片,
+                (
+                    self.矩形.x - int(self.偏移[0] * self.缩放),
+                    self.矩形.y - int(self.偏移[1] * self.缩放),
+                    # self.矩形.x,
+                    # self.矩形.y,
+                )
+                )
